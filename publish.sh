fcfcea3cc2824248116c0675b097d529241b65a8
Jester <thettboy11@gmail.com>
2023-12-24 23:55:31 +0200

    Aboba

--- publish.sh
+++ publish.sh
@@ -13,13 +13,15 @@ cd docs/.vuepress/dist
 # echo 'www.example.com' > CNAME
 
 git init
+git checkout -b gh-pages
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/v-jester/edu_db_labs master:gh-pages
+git remote add origin https://github.com/v-jester/edu_db_labs
+git push -u -f origin gh-pages
+
 
 cd -
